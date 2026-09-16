3cdc86724e4f5f035734e91b8845750695414bae
Zyragoon <Zyragoon@gmail.com>
2026-09-16 15:01:00 -0600

    finishing the second ponder for this week

---
+++ week1/ponder/variables.js
@@ -0,0 +1,18 @@
+// declare a variable
+let age = 23;
+
+console.log(age);
+ // output should be 23
+
+const name = "Zyra";
+
+ console.log(name);
+
+// Scope = where you can reference a variable in your code.
+  
+    let favoriteColor = "blue";
+    console.log(favoriteColor);   
+
+document.querySelector('h1').style.color = favoriteColor;
+
+// console.log(favoriteColor);  this will not work because favoriteColor is only available in the if statement scope.
