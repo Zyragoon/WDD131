c139aa54e0a64886f52b71a669c4198d7918ed98
Zyragoon <Zyragoon@gmail.com>
2026-09-23 14:48:22 -0600

    finishing the 2nd ponder of the week

---
+++ week2/ponders/ponder2/dom-theme.js
@@ -0,0 +1,36 @@
+/*function changeTheme(themechange) 
+{
+    console.log(themechange.target.value);
+
+
+}
+    
+let select = document.querySelector('#theme-select');
+select.addEventListener('change', changeTheme);*/
+
+
+
+
+let selectElem = document.querySelector('#theme-select');
+let pageContent = document.querySelector('body');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current === 'ocean') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
+        pageContent.style.fontFamily = "Papyrus, fantasy";
+    } else if (current === 'forest') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
+        pageContent.style.fontFamily = "Impact, sans-serif";
+    } else if (current === 'desert') {
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
+        pageContent.style.fontFamily = "'Big Caslon', serif";
+    } else {
+        // default
+        document.body.style.backgroundImage = "none";
+        pageContent.style.fontFamily = "Georgia, serif";
+    }
+}
+          
